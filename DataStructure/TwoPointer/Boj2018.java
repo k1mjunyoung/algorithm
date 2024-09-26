f588854b5c2223fc9cd0d4d28f990add727b93cb
k1mjunyoung <me@kimjunyoung.com>
2024-09-26 11:45:30 +0900

    자료구조 투포인터 2018번 문제 주석 수정

--- DataStructure/TwoPointer/Boj2018.java
+++ DataStructure/TwoPointer/Boj2018.java
@@ -1,3 +1,7 @@
+package DataStructure.TwoPointer;
+
+import java.util.Scanner;
+
 /**
  * https://www.acmicpc.net/problem/2018
  *
@@ -8,10 +12,6 @@
  * 반면에 10을 나타내는 방법은 10, 1+2+3+4의 2가지가 있다.
  * N을 입력받아 가지수를 출력하는 프로그램을 작성하시오.
  */
-package DataStructure;
-
-import java.util.Scanner;
-
 public class Boj2018 {
     public static void main(String[] args) {
         int cnt = 0;
